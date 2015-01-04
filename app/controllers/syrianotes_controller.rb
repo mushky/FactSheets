e80cc5a418ddd01cd209a0f97049108bdffa0bb3
Ali Zaidi <msaZaidi@gmail.com>
2015-01-04 02:27:56 -0800

    Allow non-users to view index and show pages

--- app/controllers/syrianotes_controller.rb
+++ app/controllers/syrianotes_controller.rb
@@ -1,5 +1,5 @@
 class SyrianotesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show] 
   
   def index
     if params[:search]
