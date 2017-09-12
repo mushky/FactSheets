8ee8aecdb5b191309728a61a2ae3f80e18cf36f5
Ali Zaidi <msazaidi@gmail.com>
2017-09-11 21:38:19 -0700

    Attempt to fix coderay formatting error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,32 @@
 module ApplicationHelper
-	class CodeRayify < Redcarpet::Render::HTML
-	  def block_code(code, language)
-	    CodeRay.scan(code, language).div
-	  end
-	end
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
 
-	def markdown(text)
-	  language ||= :plaintext		
-	  coderayified = CodeRayify.new(:filter_html => true, 
-	                                :hard_wrap => true)
-	  options = {
-	    :fenced_code_blocks => true,
-	    :no_intra_emphasis => true,
-	    :autolink => true,
-	    :strikethrough => true,
-	    :lax_html_blocks => true,
-	    :superscript => true
-	  }
-	  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-	  #debugger
-	  markdown_to_html.render(text).html_safe
-	end
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
 
-	markdown(code)
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
 
-end
-
-
+    markdown.render(text).html_safe
+  end
+	
 	# class CodeRayify < Redcarpet::Render::HTML
 	#   def block_code(code, language)
 	#     CodeRay.scan(code, language).div
 	#   end
 	# end
+
+
 
 	# def markdown(text)
 	#   language ||= :plaintext		
@@ -47,3 +42,4 @@ end
 	#   markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
 	#   markdown_to_html.render(text).html_safe
 	# end
+end
