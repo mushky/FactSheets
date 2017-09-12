b4b47bd3a4b573b85ec5ea49bcfeb8847bcb7e08
Ali Zaidi <msazaidi@gmail.com>
2017-09-11 21:30:43 -0700

    Attempt to fix coderay formatting error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,15 +10,40 @@ module ApplicationHelper
 	  coderayified = CodeRayify.new(:filter_html => true, 
 	                                :hard_wrap => true)
 	  options = {
-	    :fenced_code_blocks => false,
+	    :fenced_code_blocks => true,
 	    :no_intra_emphasis => true,
 	    :autolink => true,
+	    :strikethrough => true,
 	    :lax_html_blocks => true,
+	    :superscript => true
 	  }
-	  
 	  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+	  #debugger
 	  markdown_to_html.render(text).html_safe
 	end
 
+	markdown(code)
 
 end
+
+
+	# class CodeRayify < Redcarpet::Render::HTML
+	#   def block_code(code, language)
+	#     CodeRay.scan(code, language).div
+	#   end
+	# end
+
+	# def markdown(text)
+	#   language ||= :plaintext		
+	#   coderayified = CodeRayify.new(:filter_html => true, 
+	#                                 :hard_wrap => true)
+	#   options = {
+	#     :fenced_code_blocks => false,
+	#     :no_intra_emphasis => true,
+	#     :autolink => true,
+	#     :lax_html_blocks => true,
+	#   }
+	  
+	#   markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+	#   markdown_to_html.render(text).html_safe
+	# end
